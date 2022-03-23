a4f4d639b91974cf313e314a2af2fc46263f0176
DanielaIH <4427@holbertonschool.com>
2022-03-22 18:31:00 -0700

    FIX 0-display.c

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -9,12 +9,12 @@ void display_complex_number(complex c)
 {
 if (c.im == -1)
         printf("%d - i\n", c.re);
-if (c.im < 0)
+else if (c.im < 0)
 	printf("%d - %di\n", c.re, -c.im);
 if (c.im == 0)
 	printf("%d\n", c.re);
 if (c.im == 1)
 	printf("%d + i\n", c.re);
-if (c.im > 0)
+else if (c.im > 0)
 	printf("%d + %di\n", c.re, c.im);
 }
