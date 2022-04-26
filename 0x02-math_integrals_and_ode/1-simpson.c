dc6040ba9056a0e25704d982b90ab7d01bc5ff26
DanielaIH <4427@holbertonschool.com>
2022-04-25 20:18:17 -0700

    FIX solution for 1-simpson.c that calculates an integral using the Simpson Methode

--- 0x02-math_integrals_and_ode/1-simpson.c
+++ 0x02-math_integrals_and_ode/1-simpson.c
@@ -9,24 +9,39 @@
  * @steps: amount of rectangles
  * Return: sum
  */
-
 double simpson(double a, double b, int steps)
 {
-	double x = a, sum = 0.0, dx = (b - a) / steps;
-	int i;
 
-	sum += 1 / (1 + (x * x));
-	x += dx;
-	for (i = 1; i <= steps; i++)
-	{
-		if (i <= (steps - 1))
-			sum += 2 * (1 / (1 + (x * x)));
-		sum += 4 * (1 / (1 + ((((2 * x) - dx) / 2) * (((2 * x) - dx) / 2))));
-		x += dx;
-	}
-	x -= dx;
-	sum += 1 / (1 + (x * x));
-	sum *= (dx / 6);
-/*    return(sum);*/
-	return (0.759888051);
+    double x = a, sum = 0.0, dx = (b - a) / steps;
+    int i;
+    
+    sum += 1 / (1 + (x * x));
+    
+    for (i = 1; i <= (steps - 1); i++)
+    { 
+        x = a + i * dx;
+        if (i % 2 == 0)
+		    sum += 2 * (1 / (1 + (x * x)));
+        else
+            sum += 4 * (1 / (1 + (x * x)));
+    }
+    x = b;
+    sum += 1 / (1 + (x * x));
+    sum *= (dx / 3);
+    
+/* Con este algoritmo me dio exacto el resultado
+
+    sum += 1 / (1 + (x * x));
+    x += dx;
+    for(i = 1; i <= steps; i++)
+    {
+        if (i < (steps - 1))
+            sum += 2 * (1 / (1 + (x * x)));
+        sum += 4 * (1 / (1 + pow((((2 * x) - dx) / 2), 2)));
+        x += dx;
+    }
+    sum += 1 / (1 + (x * x));
+    sum *= (dx / 6); */
+    
+    return (sum);
 }
